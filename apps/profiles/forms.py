d1e09d86e9b0f54a0133213052a476fcd3629ad8
matheus <mcerozi@gmail.com>
2022-06-30 20:02:48 -0300

    profile app feat: editing profile

---
+++ apps/profiles/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from .models import Employee, Company
+
+
+class EmployeeProfileForm(forms.ModelForm):
+    class Meta:
+        model = Employee
+        fields = ('photo', 'name', 'age',
+        'tel', 'address', 'scholarship', 'role', 'about_me')
+
+        widgets = {
+            'photo': forms.ClearableFileInput(attrs={"class": "form-control"}),
+            'name': forms.TextInput(attrs={"class": "form-control", "placeholder": "Nome"}),
+            'age': forms.TextInput(attrs={"class": "form-control", "placeholder": "Idade"}),
+            'tel': forms.TextInput(attrs={"class": "form-control", "placeholder": "Telefone"}),
+            'address': forms.TextInput(attrs={"class": "form-control", "placeholder": "Endereço"}),
+            'scholarship': forms.Select(attrs={"class": "form-control"}),
+            'role': forms.Select(attrs={"class": "form-control"}),
+            'about_me': forms.TextInput(attrs={"class": "form-control", "placeholder": "Descreva-te."})
+        }
+
+class CompanyProfileForm(forms.ModelForm):
+    class Meta:
+        model = Company
+        fields = ('name', 'address', 
+        'city', 'country', 'cep', 'description')
+
+        widgets = {
+            'name': forms.TextInput(attrs={"class": "form-control", "placeholder": "Nome"}),
+            'address': forms.TextInput(attrs={"class": "form-control", "placeholder": "Endereço"}),
+            'city': forms.TextInput(attrs={"class": "form-control", "placeholder": "Cidade"}),
+            'country': forms.TextInput(attrs={"class": "form-control", "placeholder": "País"}),
+            'cep': forms.TextInput(attrs={"class": "form-control", "placeholder": "CEP"}),
+            'description': forms.TextInput(attrs={"class": "form-control", "placeholder": "Descrição. "})
+        }
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from apps.profiles.models import Employee, Company
+from .forms import EmployeeProfileForm, CompanyProfileForm
+from django.urls import reverse
+from apps.authentication.forms import CustomUserRegisterForm
 
 # Create your views here.
 def profile(request):
-    ...
+    if request.user.is_company:
+        template_name = 'company_profile'
+        company = Company.objects.get(user=request.user)
+        form = CompanyProfileForm(request.POST or None, instance=company)
+    else:
+        template_name = 'user_profile'
+        employee = Employee.objects.get(user=request.user)
+        form = EmployeeProfileForm(request.POST or None, request.FILES or None, instance=employee)
+
+    user_form = CustomUserRegisterForm(request.POST or None, instance=request.user)
+
+    if request.method == 'POST':
+        if form.is_valid():
+            print('salvo. ')
+            form.save()
+            return redirect(reverse('profile'))
+        else:
+            print(form.errors)
+
+    return render(request, f'home/{template_name}.html', {"form": form, "user_form": user_form})
