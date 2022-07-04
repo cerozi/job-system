5b0f6690efcaa0b88ae6d73d55c34c1b4327503c
matheus <mcerozi@gmail.com>
2022-07-04 18:39:17 -0300

    home app: unit tests

---
+++ apps/home/tests/unit_tests/test_views.py
@@ -0,0 +1,34 @@
+from django.test import TestCase
+from django.test.client import Client
+from apps.authentication.models import User
+from django.urls import reverse
+
+class HomeViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        self.user_employee = User.objects.create_user(username='test_employee_user',
+        email='test_employee@gmail.com', password='userpass123')
+        self.user_company = User.objects.create_user(username='test_company_user',
+        email='test_company@gmail.com', password='userpass123', is_company=True)
+
+    def test_get_method_employee_home(self):
+        self.client.login(email=self.user_employee.email, password='userpass123')
+        response = self.client.get(reverse('employee-home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.client.logout()
+
+        response = self.client.get(reverse('employee-home'))
+        self.assertEqual(response.status_code, 302)
+
+    def test_get_method_company_home(self):
+        self.client.login(email=self.user_company.email, password='userpass123')
+        response = self.client.get(reverse('company-home'))
+
+        self.assertEqual(response.status_code, 200)
+        self.client.logout()
+
+        response = self.client.get(reverse('company-home'))
+        self.assertEqual(response.status_code, 302)
