a0af3601f82ae287b241f5ef035a4d0a5a422842
matheus <mcerozi@gmail.com>
2022-07-05 16:23:16 -0300

    notifications app: updating signals unit tests

--- apps/notifications/tests/unit_tests/test_signals.py
+++ apps/notifications/tests/unit_tests/test_signals.py
@@ -15,13 +15,13 @@ class NotificationsSignalsTests(TestCase):
         scholarship="3", company=self.user_company.company)
 
     def test_signals_creating_job_notification(self):
-        self.assertEqual(len(Notification.objects.all()), 1)
+        self.assertEqual(len(self.user_company.from_user.all()), 1)
         self.job = Job.objects.create(title='job', salary="a1", description='test',
         scholarship="3", company=self.user_company.company)
-        self.assertEqual(len(Notification.objects.all()), 2)
+        self.assertEqual(len(self.user_company.from_user.all()), 2)
 
     def test_signals_creating_application_notification(self):
-        self.assertEqual(len(Notification.objects.all()), 1)
+        self.assertEqual(len(self.user_employee.from_user.all()), 0)
         self.apply = Apply.objects.create(job=self.test_job, employee=self.user_employee.employee, 
         salary="a1", experience='test')
-        self.assertEqual(len(Notification.objects.all()), 2)
+        self.assertEqual(len(self.user_employee.from_user.all()), 1)
