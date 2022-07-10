f60f97af9177c76fdd2b8e5b11a6c3c845820603
matheus <mcerozi@gmail.com>
2022-07-10 20:07:12 -0300

    docker

--- core/settings.py
+++ core/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres', 
+        'USER': 'postgres', 
+        'PASSWORD': 'postgres',
+        'HOST': 'db', 
+        'PORT': '5432',
     }
 }
 
